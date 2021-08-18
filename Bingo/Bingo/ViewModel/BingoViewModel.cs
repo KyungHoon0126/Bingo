7e50ac9cde367de09cda0c4ecf8a74968e7e0bf0
김경훈 <kidi052812@gmail.com>
2021-08-18 17:30:24 +0900

    [ADD] : Random Bingo Contents

--- Bingo/Bingo/ViewModel/BingoViewModel.cs
+++ Bingo/Bingo/ViewModel/BingoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Bingo.Model;
 using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -57,12 +58,16 @@ namespace Bingo.ViewModel
             }
         }
 
+        private int MAX_BINGO_COUNT = 9;
+        private Random random;
+        private List<string> BingoContents = new List<string>();
         private Dictionary<List<int>, bool> BingoCaseDictionary;
         private List<List<int>> BingoDirectionItems;
         #endregion
 
         #region Commands
         public ICommand CanPlayGameCommand { get; set; }
+        public ICommand ShuffleBingoItemsCommand { get; set; }
         #endregion
 
         #region Constructor
@@ -70,6 +75,7 @@ namespace Bingo.ViewModel
         {
             InitVariables();
             InitCommands();
+            SetBingoContents();
             SetBingoItems();
             SetBingoCaseItems();
             SetBingoCaseDictionaries();
@@ -80,6 +86,8 @@ namespace Bingo.ViewModel
         private void InitVariables()
         {
             BingoItems = new ObservableCollection<BingoModel>();
+            random = new Random();
+            BingoContents = new List<string>();
             BingoCaseDictionary = new Dictionary<List<int>, bool>();
             BingoDirectionItems = new List<List<int>>();
         }
@@ -87,19 +95,56 @@ namespace Bingo.ViewModel
         private void InitCommands()
         {
             CanPlayGameCommand = new DelegateCommand(OnBingoSelectionChange, CanBingoSelectionChange);
+            ShuffleBingoItemsCommand = new DelegateCommand(OnShuffleBingoItems);
+        }
+
+        private void SetBingoContents()
+        {
+            #region Set Dummy BingoContents
+            BingoContents.Add("내 책상에는 항상 카페인 음료가 있다");
+            BingoContents.Add("주변 사람들로부터 컴퓨터 관련된 질문을 받아본 적있다");
+            BingoContents.Add("영타 400타 이상이다");
+            BingoContents.Add("노트북에 스티커가 붙어 있다");
+            BingoContents.Add("어? 라는 말을 많이 한다");
+            BingoContents.Add("기계식 키보드를 갖고 있다");
+            BingoContents.Add("할 줄 아는 언어가 3개 이상이다");
+            BingoContents.Add("허리가 좋지 않다");
+            BingoContents.Add("개발과 게임, 둘 중 선택하라 하면 개발이다");
+            BingoContents.Add("큰 백팩을 자주 맨다");
+            BingoContents.Add("기능이 없어 만들어 쓴 적 있다");
+            BingoContents.Add("버그 잡다가 새로운 버그를 발견한 적 있다");
+            BingoContents.Add("후드티를 많이 갖고 있다");
+            BingoContents.Add("불금에는 코딩이지");
+            BingoContents.Add("개발 개그를 보면 공감간다");
+            BingoContents.Add("Ctrl+S를 습관적으로 누른다");
+            BingoContents.Add("칼퇴보다 야근이 더 많다");
+            BingoContents.Add("일을 시작하고 나서 손목터널증후군이 생겼다");
+            BingoContents.Add("적성에 맞는지 진지하게 생각해본 적 있다");
+            BingoContents.Add("화려하지만 심플하게라는 말을 들어봤다");
+            BingoContents.Add("외주를 받아봤다");
+            BingoContents.Add("이름만 부른 메신저가 오면 방어태세부터 갖춘다");
+            BingoContents.Add("이상한 폰트보면 기분 나빠진다");
+            BingoContents.Add("가끔 친구들이 내가 하는 말을 이해하지 못한다");
+            BingoContents.Add("자격증이 3개 이상이다");
+            BingoContents.Add("이력서 100개 이상 뿌려 봤다");
+            BingoContents.Add("낮과 밤이 바뀌었다");
+            BingoContents.Add("코드 리펙토링이 필요하지만 못 본 척 지나친적 있다");
+            BingoContents.Add("Pull Request 날렸다가 퇴짜 맞은적이 있다");
+            BingoContents.Add("자주 사용하는 개발툴이 있다");
+            #endregion
         }
 
         private void SetBingoItems()
         {
-            BingoItems.Add(new BingoModel { BingoContent = "내 책상에는 항상 카페인 음료가 있다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "주변 사람들로부터 컴퓨터 관련된 질문을 받아본 적있다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "영타 400타 이상이다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "노트북에 스티커가 붙어 있다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "어? 라는 말을 많이 한다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "기계식 키보드를 갖고 있다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "할 줄 아는 언어가 3개 이상이다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "허리가 좋지 않다", IsSelected = false });
-            BingoItems.Add(new BingoModel { BingoContent = "개발과 게임, 둘 중 선택하라 하면 개발이다", IsSelected = false });
+            while(MAX_BINGO_COUNT != 0)
+            {
+                int randomBingoIdx = random.Next(0, 30);
+                if (BingoItems.Where(x => x.BingoContent == BingoContents[randomBingoIdx]).FirstOrDefault() == null)
+                {
+                    BingoItems.Add(new BingoModel { BingoContent = BingoContents[randomBingoIdx], IsSelected = false });
+                    MAX_BINGO_COUNT -= 1;
+                }
+            }
         }
 
         private void SetBingoCaseItems()
@@ -125,6 +170,16 @@ namespace Bingo.ViewModel
         {
             SelectedBingo.IsSelected = true;
             IsMatchBingo();
+        }
+
+        private void OnShuffleBingoItems()
+        {
+            if(BingoItems.Count > 0)
+            {
+                BingoItems.Clear();
+            }
+            MAX_BINGO_COUNT = 9;
+            SetBingoItems();
         }
         #endregion
 
